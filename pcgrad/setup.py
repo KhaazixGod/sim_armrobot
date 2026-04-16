1493229335956984bfdee2684cb79ceba63de233
KhaazixGod <KhaazixGod.com>
2026-04-16 14:56:40 +0700

    create repo

---
+++ pcgrad/setup.py
@@ -0,0 +1,34 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'pcgrad'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pread',
+    maintainer_email='KhaazixGod.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'joints_state = pcgrad.joints_state:main',
+            'run_environment = pcgrad.run_environment:main',
+        ],
+    },
+)
